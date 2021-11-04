2bc63335bcc594df39c4941b443e8999600cb921
HMichaut <hectormichaut@gmail.com>
2021-11-04 22:56:03 +0100

    basic task boxes created

--- script.js
+++ script.js
@@ -40,6 +40,37 @@ const toDoList = (() => {
   };
 })();
 
+function displayProject(inputProject) {
+  const inputToDolist = inputProject.getAttributedToDoList();
+  const contentDiv = document.getElementById("content-text");
+  for (let i = 0; i < inputToDolist.length; i++) {
+    const toDoListBox = document.createElement("div");
+    toDoListBox.className = "to-do-list-box";
+
+    const titleBox = document.createElement("div");
+    titleBox.innerHTML = inputToDolist[i].getTitle();
+    titleBox.className = "param-box";
+
+    const descriptionBox = document.createElement("div");
+    descriptionBox.innerHTML = inputToDolist[i].getDescription();
+    descriptionBox.className = "param-box";
+
+    const dueDate = document.createElement("div");
+    dueDate.innerHTML = inputToDolist[i].getDueDate();
+    dueDate.className = "param-box";
+
+    const priority = document.createElement("div");
+    priority.innerHTML = inputToDolist[i].getPriority();
+    priority.className = "param-box";
+
+    toDoListBox.appendChild(titleBox);
+    toDoListBox.appendChild(descriptionBox);
+    toDoListBox.appendChild(dueDate);
+    toDoListBox.appendChild(priority);
+    contentDiv.appendChild(toDoListBox);
+  }
+};
+
 testToDo1 = toDo("test title 1", "test description 1", "test date 1", "urgent1");
 testToDo2 = toDo("test title 2", "test description 2", "test date 2", "urgent2");
 testToDo3 = toDo("test title 3", "test description 3", "test date 3", "urgent3");
@@ -55,3 +86,5 @@ testProject2 = project("test name 2", [testToDo5, testToDo6, testToDo7]);
 toDoList.addProjectToList(testProject);
 toDoList.addProjectToList(testProject2);
 console.log(toDoList.displayProjectList());
+
+displayProject(testProject2);
